5a095d9d31f066b24db2a2df0f283c9765329c60
aashaym1 <aashaym1@gmail.com>
2020-05-16 20:48:10 +0530

    Extract the Ndn-tools zip and paste the bbr files within the catchunks folder after their completion the hpp file is almost complete but the cpp file still has some work to be done

---
+++ pipeline-interests-bbr.hpp
@@ -0,0 +1,68 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2016-2019, Regents of the University of California,
+ *                          Colorado State University,
+ *                          University Pierre & Marie Curie, Sorbonne University.
+ *
+ * This file is part of ndn-tools (Named Data Networking Essential Tools).
+ * See AUTHORS.md for complete list of ndn-tools authors and contributors.
+ *
+ * ndn-tools is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * ndn-tools is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * ndn-tools, e.g., in COPYING.md file.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * See AUTHORS.md for complete list of ndn-cxx authors and contributors.
+ *
+ *
+ */
+
+#ifndef NDN_TOOLS_CHUNKS_CATCHUNKS_PIPELINE_INTERESTS_BBR_HPP
+#define NDN_TOOLS_CHUNKS_CATCHUNKS_PIPELINE_INTERESTS_BBR_HPP
+
+#include "pipeline-interests-adaptive.hpp"
+
+namespace ndn {
+namespace chunks {
+
+/**
+ * @brief Implements The Four Phases in BBR V1.
+ *
+ * This implementation follows the RFC 
+ * and the Linux kernel implementation 
+ */
+class PipelineInterestsBBR final : public PipelineInterestsAdaptive
+{
+public:
+  PipelineInterestsBBR(Face& face, RttEstimatorWithStats& rttEstimator, const Options& opts);
+
+private:
+  void
+  Startup() final;
+
+  void
+  Drain() final;
+  
+  void
+  ProbeBw() final;
+  
+  void
+  ProbeRtt() final;
+
+
+private:
+  double m_wmax = 0.0; ///< window size before last window decrease
+  double m_lastWmax = 0.0; ///< last wmax
+  time::steady_clock::TimePoint m_lastDecrease; ///< time of last window decrease
+};
+
+} // namespace chunks
+} // namespace ndn
+
+#endif // NDN_TOOLS_CHUNKS_CATCHUNKS_PIPELINE_INTERESTS_BBR_HPP
